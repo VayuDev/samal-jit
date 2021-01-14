VayuDev/samal-jit

commit b3bf37ff1097141074b47a21629e8834d24b1d6e

Lang: Implement tuple access syntax (tuple:1) & fix vm for it
The syntax uses a colon instead of a dot because the dot gets parsed as
the identifier name right now - this should be fixed in the parser soon.

This patch also fixes the REPUSH_FROM_N instruction in JIT mode, which
only caused a problem when you repush more than 8 bytes at once (which
is right now necessary for tuple access).

TODO don't repush the whole tuple if it's referred by it's name and
instead fetch the accessed element directly from where it is stored on
the stack.

samal_lib/lib/VM.cpp
@@ -79,7 +79,6 @@ public:
         sub(rsp, stackSize);
 
 
-        std();
         auto jumpWithIp = [&] {
           jmp(ptr [tableRegister + ip * sizeof(void*)]);
         };
@@ -172,6 +171,7 @@ public:
                 add(rsi, repushOffset);
                 sub(rsp, repushLen);
                 mov(rdi, rsp);
+                cld();
                 //    copy
                 for (int j = 0; j < repushLen / 8; ++j) {
                     movsq();
@@ -189,6 +189,7 @@ public:
                 mov(rdi, rsp);
                 add(rdi, popOffset + popLen - 8);
 
+                std();
                 //    copy
                 for (int j = 0; j < popOffset / 8; ++j) {
                     movsq();
@@ -243,6 +244,7 @@ public:
                     mov(rdi, rsp);
                     add(rdi, popOffset + popLen - 8);
 
+                    std();
                     //    copy
                     for (int j = 0; j < popOffset / 8; ++j) {
                         movsq();

samal_lib/include/samal_lib/AST.hpp
@@ -231,6 +231,19 @@ private:
     up<ExpressionNode> mName, mIndex;
 };
 
+class TupleAccessExpressionNode : public ExpressionNode {
+public:
+    TupleAccessExpressionNode(SourceCodeRef source, up<ExpressionNode> name, uint32_t index);
+    [[nodiscard]] std::optional<Datatype> getDatatype() const override;
+    void completeDatatype(DatatypeCompleter& declList) override;
+    void compile(Compiler &) const override;
+    [[nodiscard]] std::string dump(unsigned indent) const override;
+    [[nodiscard]] inline const char* getClassName() const override { return "TupleAccessExpressionNode"; }
+private:
+    up<ExpressionNode> mName;
+    uint32_t mIndex;
+};
+
 class DeclarationNode : public ASTNode {
 public:
     explicit DeclarationNode(SourceCodeRef source);

samal_lib/include/samal_lib/Compiler.hpp
@@ -58,7 +58,7 @@ public:
     void binaryOperation(const Datatype& inputTypes, BinaryExpressionNode::BinaryOperator op);
     void performFunctionCall(size_t sizeOfArguments, size_t sizeOfReturnValue);
     void setVariableLocation(const up<IdentifierNode>& identifier);
-
+    void accessTupleElement(const Datatype& tupleType, uint32_t index);
 private:
     void addInstructions(Instruction insn);
     void addInstructions(Instruction insn, int32_t param);

samal_lib/lib/AST.cpp
@@ -519,6 +519,39 @@ std::string ListAccessExpressionNode::dump(unsigned int indent) const {
     return ret;
 }
 
+TupleAccessExpressionNode::TupleAccessExpressionNode(SourceCodeRef source, up<ExpressionNode> name, uint32_t index)
+: ExpressionNode(std::move(source)), mName(std::move(name)), mIndex(index) {
+
+}
+std::optional<Datatype> TupleAccessExpressionNode::getDatatype() const {
+    auto nameType = mName->getDatatype();
+    if(!nameType)
+        return {};
+    return nameType->getTupleInfo().at(mIndex);
+}
+void TupleAccessExpressionNode::completeDatatype(DatatypeCompleter& declList) {
+    mName->completeDatatype(declList);
+    auto lhsType = mName->getDatatype();
+    assert(lhsType);
+    if(lhsType->getCategory() != DatatypeCategory::tuple) {
+        throwException("Trying to access non-tuple type " + lhsType->toString() + " as a tuple");
+    }
+    // TODO detect out of range
+}
+void TupleAccessExpressionNode::compile(Compiler& comp) const {
+    // TODO optimize if lhs is IdentifierNode
+    mName->compile(comp);
+    comp.accessTupleElement(*mName->getDatatype(), mIndex);
+}
+std::string TupleAccessExpressionNode::dump(unsigned int indent) const {
+    auto ret = ASTNode::dump(indent);
+    ret += createIndent(indent + 1) + "Name:\n";
+    ret += mName->dump(indent + 2);
+    ret += createIndent(indent + 1) + "Index:\n";
+    ret += std::to_string(mIndex);
+    return ret;
+}
+
 DeclarationNode::DeclarationNode(SourceCodeRef source)
 : ASTNode(std::move(source)) {
 }

samal_lib/lib/Compiler.cpp
@@ -161,6 +161,20 @@ void* Compiler::getLabelPtr(size_t label) {
 void Compiler::changeStackSize(ssize_t diff) {
     mStackSize += diff;
 }
+void Compiler::accessTupleElement(const Datatype& tupleType, uint32_t index) {
+    auto& tupleInfo = tupleType.getTupleInfo();
+    auto& accessedType = tupleInfo.at(index);
+    size_t offsetOfAccessedType = 0;
+    for(ssize_t i = static_cast<ssize_t>(tupleInfo.size()) - 1; i > index; --i) {
+        offsetOfAccessedType += tupleInfo.at(i).getSizeOnStack();
+    }
+    addInstructions(Instruction::REPUSH_FROM_N, accessedType.getSizeOnStack(), offsetOfAccessedType);
+    addInstructions(Instruction::POP_N_BELOW, tupleType.getSizeOnStack(), accessedType.getSizeOnStack());
+
+    mStackSize += accessedType.getSizeOnStack();
+    mStackSize -= tupleType.getSizeOnStack();
+
+}
 
 FunctionDuration::FunctionDuration(Compiler& compiler, const up<IdentifierNode>& identifier, const up<ParameterListNode>& params)
 : mCompiler(compiler), mIdentifier(identifier), mParams(params) {

samal_lib/lib/Parser.cpp
@@ -195,7 +195,7 @@ Parser::Parser() {
         };
     };
     // this is stupid because we don't handle left recursion correctly in the peg parser :c
-    mPegParser["PostfixExpression"] << "LiteralExpression ~nws~(~nws~(~nws~'(' ExpressionListWithoutDatatype ')') | ~nws~('[' Expression ']'))*" >> [](peg::MatchInfo& res) -> peg::Any {
+    mPegParser["PostfixExpression"] << "LiteralExpression ~nws~(~nws~(~nws~'(' ExpressionListWithoutDatatype ')') | ~nws~('[' Expression ']') | ~nws~(~nws~':' ~nws~[\\d]+))*" >> [](peg::MatchInfo& res) -> peg::Any {
         peg::Any ret = std::move(res[0].result);
         while (!res[1].subs.empty()) {
             switch (*res[1][0].choice) {
@@ -213,6 +213,19 @@ Parser::Parser() {
                     up<ExpressionNode> { res[1][0][0][1].result.move<ExpressionNode*>() }
                 };
                 break;
+            case 2: {
+                uint32_t index;
+                const auto& node = res[1][0][0][1];
+                std::from_chars(node.start, node.start + node.len, index);
+                ret = TupleAccessExpressionNode {
+                    toRef(res),
+                    up<ExpressionNode> { ret.move<ExpressionNode*>() },
+                    index
+                };
+                break;
+            }
+            default:
+                assert(false);
             }
             res[1].subs.erase(res[1].subs.begin());
         }

commit da67ab6027f8ecd554c462ffe6aeefe845dc8c7c

Lang: Fix jit performance regression introduced by b3bf37ff10
This code is even about 30% faster than before b3bf37ff10 in the fib(28)
benchmark.

I noticed that performance was about twice as bad after b3bf37ff10 as it
had been before that patch, but that patch only introduced 1 x86
instruction for 3 of the bytecode instructions. The real reason for the
large regression seems to be the fact that movsq() is faster with
the direction flag (std()) set than without it (cld()).

This patch replaces the code that previously needed a cleared direction
flag (cld()) with one that works without movsq and only uses mov and
push.

samal_lib/lib/VM.cpp
@@ -80,6 +80,7 @@ public:
         //    adjust rsp
         sub(rsp, stackSize);
 
+        std();
         auto jumpWithIp = [&] {
             jmp(ptr[tableRegister + ip * sizeof(void*)]);
         };
@@ -166,22 +167,18 @@ public:
             case Instruction::REPUSH_FROM_N: {
                 int32_t repushLen = *(uint32_t*)&instructions.at(i + 1);
                 int32_t repushOffset = *(uint32_t*)&instructions.at(i + 5);
-                //    init copy
                 assert(repushLen % 8 == 0);
-                mov(rsi, rsp);
-                add(rsi, repushOffset);
-                sub(rsp, repushLen);
-                mov(rdi, rsp);
-                cld();
-                //    copy
                 for(int j = 0; j < repushLen / 8; ++j) {
-                    movsq();
+                    mov(rax, qword[rsp + (repushOffset + repushLen - 8)]);
+                    push(rax);
                 }
                 break;
             }
             case Instruction::POP_N_BELOW: {
                 int32_t popLen = *(uint32_t*)&instructions.at(i + 1);
                 int32_t popOffset = *(uint32_t*)&instructions.at(i + 5);
+                // TODO replace movsq()-code with simple mov code, similar to REPUSH_FROM_N
+
                 //    init copy
                 assert(popOffset % 8 == 0);
                 mov(rsi, rsp);
@@ -190,7 +187,6 @@ public:
                 mov(rdi, rsp);
                 add(rdi, popOffset + popLen - 8);
 
-                std();
                 //    copy
                 for(int j = 0; j < popOffset / 8; ++j) {
                     movsq();
@@ -243,7 +239,6 @@ public:
                     mov(rdi, rsp);
                     add(rdi, popOffset + popLen - 8);
 
-                    std();
                     //    copy
                     for(int j = 0; j < popOffset / 8; ++j) {
                         movsq();