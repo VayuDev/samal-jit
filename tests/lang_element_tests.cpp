59fc03febaf3a2c37d3e47d05b93931492a1ccbc
VayuDev <vayudev@protonmail.com>
2021-04-12 18:11:26 +0200

    Tests: Fix Datatype type inference test for new API syntax

--- tests/lang_element_tests.cpp
+++ tests/lang_element_tests.cpp
@@ -14,15 +14,15 @@ TEST_CASE("Ensure that template type inference works 1", "[samal_element_tests]"
     Datatype incompleteType = Datatype::createTupleType({ Datatype::createUndeterminedIdentifierType("T"), Datatype::createUndeterminedIdentifierType("S") });
     Datatype fullType = Datatype::createTupleType({ Datatype::createSimple(samal::DatatypeCategory::i32), Datatype::createSimple(samal::DatatypeCategory::i64)});
     UndeterminedIdentifierReplacementMap map;
-    incompleteType.inferTemplateTypes(fullType, map);
+    incompleteType.inferTemplateTypes(fullType, map, {});
     REQUIRE(map.size() == 2);
-    REQUIRE(map.at("T").first == Datatype::createSimple(samal::DatatypeCategory::i32));
-    REQUIRE(map.at("S").first == Datatype::createSimple(samal::DatatypeCategory::i64));
+    REQUIRE(map.at("T").type == Datatype::createSimple(samal::DatatypeCategory::i32));
+    REQUIRE(map.at("S").type == Datatype::createSimple(samal::DatatypeCategory::i64));
 }
 
 TEST_CASE("Ensure that template type inference works 2", "[samal_element_tests]") {
     Datatype incompleteType = Datatype::createTupleType({ Datatype::createUndeterminedIdentifierType("T"), Datatype::createUndeterminedIdentifierType("S") });
     Datatype fullType = Datatype::createSimple(samal::DatatypeCategory::i64);
     UndeterminedIdentifierReplacementMap map;
-    REQUIRE_THROWS(incompleteType.inferTemplateTypes(fullType, map));
+    REQUIRE_THROWS(incompleteType.inferTemplateTypes(fullType, map, {}));
 }
